eaf61e5768e14c6664ecbc321d1290ac2f180406
Max Starr <max@maxstarr.xyz>
2022-01-11 12:47:33 -0600

    falling blocks

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,7 @@ console.log("🚀 ~ file: index.ts ~ line 24 ~ board", board);
 
 ctx.fillStyle = BACKGROUND_COLOR;
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.font = "30px Arial";
 
 draw();
 
@@ -38,8 +39,22 @@ canvas.addEventListener("click", (e) => {
   const y = Math.floor((HEIGHT - e.offsetY) / TILE_SIZE);
   if (board[y][x] === 0) return;
   board[y][x] = 0;
+  updatePositions();
   draw();
 });
+
+function updatePositions() {
+  for (let y = board.length - 1; y >= 0; y--) {
+    for (let x = 0; x < board[y].length; x++) {
+      if (board[y][x] !== 0) continue;
+      for (let yAbove = y + 1; yAbove < board.length; yAbove++) {
+        // if (board[yAbove][x] === 0) break;
+        board[yAbove - 1][x] = board[yAbove][x];
+      }
+    }
+  }
+}
+
 function draw() {
   for (let y = board.length - 1; y >= 0; y--) {
     for (let x = 0; x < board[y].length; x++) {
@@ -48,6 +63,13 @@ function draw() {
         x * TILE_SIZE,
         HEIGHT - (y + 1) * TILE_SIZE,
         TILE_SIZE,
+        TILE_SIZE,
+      );
+      ctx.fillStyle = "black";
+      ctx?.fillText(
+        `${x},${y}`,
+        x * TILE_SIZE + TILE_SIZE / 2,
+        HEIGHT - (y + 1) * TILE_SIZE + TILE_SIZE / 2,
         TILE_SIZE,
       );
     }
