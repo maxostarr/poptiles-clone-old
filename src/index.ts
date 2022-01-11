ce655d2a18f5989e42bef7ed153a4afded3f0a57
Max Starr <max@maxstarr.xyz>
2022-01-11 12:26:02 -0600

    random color map

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 const WIDTH = 700;
 const HEIGHT = WIDTH * 1.5;
+const TILE_SIZE = 100;
+const COLOR_MAP = ["#ff0000", "#ff8000", "#ffff00", "#80ff00", "#00ff00"];
 
 const canvas = document.getElementById("mainGame") as HTMLCanvasElement;
 canvas.width = WIDTH;
@@ -9,5 +11,17 @@ const ctx = canvas.getContext("2d");
 if (!ctx) {
   throw new Error("Could not get context");
 }
-ctx.fillStyle = "green";
+
+const board = Array.from({ length: HEIGHT / TILE_SIZE }, () =>
+  Array.from({ length: WIDTH / TILE_SIZE }, () => (Math.random() * 6) | 0),
+);
+
+ctx.fillStyle = "#eee";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+for (let y = 0; y < board.length; y++) {
+  for (let x = 0; x < board[y].length; x++) {
+    ctx.fillStyle = COLOR_MAP[board[y][x]];
+    ctx.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+  }
+}
