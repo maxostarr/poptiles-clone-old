96f94aeec180d28e19e6f1e531db1c1ae63b8825
Max Starr <max@maxstarr.xyz>
2022-01-12 12:17:17 -0600

    find threes semi working

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ const TILE_SIZE = 100;
 const BACKGROUND_COLOR = "#eee";
 const COLOR_MAP = [
   BACKGROUND_COLOR,
-  "#ff0000",
-  "#ff8000",
-  "#ffff00",
-  "#80ff00",
-  "#00ff00",
+  "#87FFE5",
+  "#F3D2FC",
+  "#70BA5B",
+  "#B559CF",
+  "#C42145",
   "#00ff80",
 ];
 const NEIGHBOR_OFFSETS = [
@@ -29,7 +29,7 @@ if (!ctx) {
 
 const board = Array.from({ length: HEIGHT / TILE_SIZE }, (_, row) =>
   Array.from({ length: WIDTH / TILE_SIZE }, () =>
-    Number(row < 3 && (Math.random() * (COLOR_MAP.length - 1) + 1) | 0),
+    Number(row < 6 && (Math.random() * (COLOR_MAP.length - 1) + 1) | 0),
   ),
 );
 
@@ -39,16 +39,107 @@ ctx.font = "30px Arial";
 
 draw();
 
-canvas.addEventListener("click", (e) => {
+canvas.addEventListener("click", async (e) => {
   const x = Math.floor(e.offsetX / TILE_SIZE);
   const y = Math.floor((HEIGHT - e.offsetY) / TILE_SIZE);
   if (board[y][x] === 0) return;
-  removeTile(y, x);
+  removeTileGroupAround(y, x);
+  updatePositions();
+  draw();
+  highlightTiles(await getThreesToRemove());
+  await delay(1000);
+  await removeThrees();
   updatePositions();
   draw();
 });
 
-function removeTile(y: number, x: number) {
+async function getThreesToRemove() {
+  let tilesToRemove: string[] = [];
+  for (let y = 0; y < board.length; y++) {
+    for (let x = 0; x < board[y].length; x++) {
+      if (board[y][x] === 0) continue;
+      let offset1 = NEIGHBOR_OFFSETS[0];
+      let offset2 = NEIGHBOR_OFFSETS[3];
+      let [neighbor1Y, neighbor1X] = [y + offset1[0], x + offset1[1]];
+      let [neighbor2Y, neighbor2X] = [y + offset2[0], x + offset2[1]];
+
+      if (
+        neighbor1X < 0 ||
+        neighbor1Y < 0 ||
+        neighbor2Y < 0 ||
+        neighbor2Y < 0 ||
+        board[neighbor1Y][neighbor1X] === 0 ||
+        board[neighbor2Y][neighbor2X] === 0
+      )
+        continue;
+      // highlightTiles([
+      //   getTileHash(y, x),
+      //   getTileHash(neighbor1Y, neighbor1X),
+      //   getTileHash(neighbor2Y, neighbor2X),
+      // ]);
+      // console.log(
+      //   `${y},${x}: neighbor1: ${neighbor1Y},${neighbor1X}: neighbor2: ${neighbor2Y},${neighbor2X}`,
+      // );
+      // await delay(1000);
+      // draw();
+      if (
+        board[neighbor1Y][neighbor1X] === board[y][x] &&
+        board[neighbor2Y][neighbor2X] === board[y][x]
+      ) {
+        tilesToRemove = [...tilesToRemove, ...getTilesToRemoveAround(y, x)];
+      }
+
+      offset1 = NEIGHBOR_OFFSETS[1];
+      offset2 = NEIGHBOR_OFFSETS[2];
+      [neighbor1Y, neighbor1X] = [y + offset1[0], x + offset1[1]];
+      [neighbor2Y, neighbor2X] = [y + offset2[0], x + offset2[1]];
+
+      highlightTiles([
+        getTileHash(y, x),
+        getTileHash(neighbor1Y, neighbor1X),
+        getTileHash(neighbor2Y, neighbor2X),
+      ]);
+      console.log(
+        `${y},${x}: neighbor1: ${neighbor1Y},${neighbor1X}: neighbor2: ${neighbor2Y},${neighbor2X}`,
+      );
+      await delay(1000);
+      draw();
+      if (
+        neighbor1X < 0 ||
+        neighbor1Y < 0 ||
+        neighbor2Y < 0 ||
+        neighbor2Y < 0 ||
+        board[neighbor1Y][neighbor1X] === 0 ||
+        board[neighbor2Y][neighbor2X] === 0
+      )
+        continue;
+      if (
+        board[neighbor1Y][neighbor1X] === board[y][x] &&
+        board[neighbor2Y][neighbor2X] === board[y][x]
+      ) {
+        tilesToRemove = [...tilesToRemove, ...getTilesToRemoveAround(y, x)];
+      }
+    }
+  }
+  return tilesToRemove;
+}
+
+async function removeThrees() {
+  removeTiles(await getThreesToRemove());
+}
+
+function removeTileGroupAround(y: number, x: number) {
+  removeTiles(getTilesToRemoveAround(y, x));
+}
+
+function removeTiles(tileHashes: string[]) {
+  tileHashes.forEach((hash) => {
+    const [y, x] = getCoordsFromHash(hash);
+    board[y][x] = 0;
+  });
+}
+
+function getTilesToRemoveAround(y: number, x: number) {
   const tileQueue = [getTileHash(y, x)];
   const tilesToRemove = [getTileHash(y, x)];
   while (tileQueue.length > 0) {
@@ -69,11 +160,7 @@ function removeTile(y: number, x: number) {
       }
     }
   }
-
-  tilesToRemove.forEach((hash) => {
-    const [y, x] = getCoordsFromHash(hash);
-    board[y][x] = 0;
-  });
+  return tilesToRemove;
 }
 
 function updatePositions() {
@@ -89,6 +176,7 @@ function updatePositions() {
 }
 
 function draw() {
+  if (!ctx) return;
   for (let y = board.length - 1; y >= 0; y--) {
     for (let x = 0; x < board[y].length; x++) {
       ctx.fillStyle = COLOR_MAP[board[y][x]];
@@ -99,7 +187,7 @@ function draw() {
         TILE_SIZE,
       );
       ctx.fillStyle = "black";
-      ctx?.fillText(
+      ctx.fillText(
         `${x},${y}`,
         x * TILE_SIZE + TILE_SIZE / 2,
         HEIGHT - (y + 1) * TILE_SIZE + TILE_SIZE / 2,
@@ -116,4 +204,25 @@ function getTileHash(y: number, x: number) {
 function getCoordsFromHash(hash: string) {
   const [y, x] = hash.split("-").map((str) => Number(str));
   return [y, x];
+}
+
+function highlightTiles(tiles: string[]) {
+  if (!ctx) return;
+  tiles.forEach((hash) => {
+    const [y, x] = getCoordsFromHash(hash);
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 3;
+    ctx.strokeRect(
+      x * TILE_SIZE,
+      HEIGHT - (y + 1) * TILE_SIZE,
+      TILE_SIZE,
+      TILE_SIZE,
+    );
+  });
+}
+
+function delay(time: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
 }
